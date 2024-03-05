d437282697cec37d8b79ffe991fe9fc30be8539c
Abdi Berhe <abdiberhe@gmail.com>
2024-03-05 11:57:23 +0300

    feat: Function that handles multiple promises using Promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(values[0].body, values[1].firstName, values[1].lastName);
     })
