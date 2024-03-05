1eeb3d6a866f290eb2b7eb288d41c3a6477d0d9c
Abdi Berhe <abdiberhe@gmail.com>
2024-03-05 12:48:04 +0300

    feat: Function that handles multple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,10 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => {
+  ]).then((values) => {
     const arr = [];
-    for (const result of results) {
-      arr.push({ status: result.status, value: result.value || result.reason });
+    for (const item of values) {
+      arr.push({ status: item.status, value: item.value || item.reason });
     }
     return arr;
   });
