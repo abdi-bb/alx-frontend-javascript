cec37959aaa20f6c0837708efcb9e88b5b8fb319
Abdi Berhe <abdiberhe@gmail.com>
2024-03-05 12:44:12 +0300

    feat: Function that handles multple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,10 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
-
-  return Promise.allSettled([promise1, promise2]).then((results) => {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => {
     const arr = [];
     for (const result of results) {
       arr.push({ status: result.status, value: result.value || result.reason });
